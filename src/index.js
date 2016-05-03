0b8d957b3949cd966d921e41b14c5379ec3f8d89
John-David Dalton <john.david.dalton@gmail.com>
2016-05-03 10:10:37 -0700

    Use class syntax.

--- src/index.js
+++ src/index.js
@@ -2,36 +2,36 @@ import _ from 'lodash';
 import path from 'path';
 import { features } from './mapping';
 
-function LodashReplacementPlugin(options={}) {
-  const replacements = this.replacements = [];
-  _.forOwn(features, (pairs, key) => {
-    if (!options[key]) {
-      _.each(pairs, pair => {
-        const pattern = RegExp(`\\/lodash(?:-es)?\\/${ pair[0] }\\.js$`);
-        replacements.push([pattern, `./${ pair[1] }.js`]);
-      });
-    }
-  });
-}
-
-LodashReplacementPlugin.prototype.apply = function(compiler) {
-  const replacements = this.replacements;
-  compiler.plugin('normal-module-factory', nmf => {
-    nmf.plugin('after-resolve', (result, callback) => {
-      if (!result) {
-        return callback();
+export default class LodashReplacementPlugin {
+  constructor(options={}) {
+    const replacements = this.replacements = [];
+    _.forOwn(features, (pairs, key) => {
+      if (!options[key]) {
+        _.each(pairs, pair => {
+          const pattern = RegExp(`\\/lodash(?:-es)?\\/${ pair[0] }\\.js$`);
+          replacements.push([pattern, `./${ pair[1] }.js`]);
+        });
       }
-      let length = replacements.length;
-      while (length--) {
-        const pair = replacements[length];
-        if (pair[0].test(result.resource)) {
-          result.resource = path.resolve(path.dirname(result.resource), pair[1]);
-          break;
-        }
-      }
-      return callback(null, result);
     });
-  });
-};
+  }
 
-module.exports = LodashReplacementPlugin;
+  apply(compiler) {
+    const replacements = this.replacements;
+    compiler.plugin('normal-module-factory', nmf => {
+      nmf.plugin('after-resolve', (result, callback) => {
+        if (!result) {
+          return callback();
+        }
+        let length = replacements.length;
+        while (length--) {
+          const pair = replacements[length];
+          if (pair[0].test(result.resource)) {
+            result.resource = path.resolve(path.dirname(result.resource), pair[1]);
+            break;
+          }
+        }
+        return callback(null, result);
+      });
+    });
+  }
+};
