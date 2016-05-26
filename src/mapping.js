358502e6d789a0fb35801d15c2dddf9023d7378c
John-David Dalton <john.david.dalton@gmail.com>
2016-05-26 07:41:40 -0700

    Add `isSymbol` to `uncommon` mapping.

--- src/mapping.js
+++ src/mapping.js
@@ -96,6 +96,7 @@ export const features = {
     ['_initCloneByTag', 'identity'],
     ['isArguments', 'stubFalse'],
     ['isArguments', 'noop'],
+    ['isSymbol', 'stubFalse'],
     ['isTypedArray', 'stubFalse']
   ],
 };
