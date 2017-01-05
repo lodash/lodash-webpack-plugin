b1f949a9caff05f51f40bd458078a720181c721f
John-David Dalton <john.david.dalton@gmail.com>
2017-01-05 10:04:24 -0800

    Add test for `paths` overrides mapping entry.

--- test/index.js
+++ test/index.js
@@ -183,4 +183,22 @@ describe('reduced modular builds', function() {
         .catch(done);
     });
   });
+
+  /*--------------------------------------------------------------------------*/
+
+  _.each(glob.sync(path.join(__dirname, 'path-fixtures/*/')), testPath => {
+    const testName = getTestName(testPath);
+    const actualPath = path.join(testPath, 'actual.js');
+    const config = new Config(actualPath);
+    const plugin = config.plugins[0];
+
+    it(`should enable paths for explicit \`${ testName }\` use`, done => {
+      new Compiler(config).run()
+        .then(() => {
+          assert.ok(!_.some(plugin.matches, pair => _.endsWith(pair[0], '_castPath.js')));
+          done();
+        })
+        .catch(done);
+    });
+  });
 });
---
+++ test/path-fixtures/pick/actual.js
@@ -0,0 +1,3 @@
+import { pick } from 'lodash';
+
+pick({ 'a': 1, 'b': 2 }, ['a']);
